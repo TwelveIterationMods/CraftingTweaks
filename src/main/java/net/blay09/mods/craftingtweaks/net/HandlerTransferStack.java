8aafed130ff2cae2096d9f86d8fe3b4ff98f6124
Blay09 <blay09@hotmail.de>
2015-12-29 16:32:01 +0100

    Fix NPE

--- src/main/java/net/blay09/mods/craftingtweaks/net/HandlerTransferStack.java
+++ src/main/java/net/blay09/mods/craftingtweaks/net/HandlerTransferStack.java
@@ -19,10 +19,14 @@ public class HandlerTransferStack implements IMessageHandler<MessageTransferStac
         if(container != null && message.slotNumber >= 0 && message.slotNumber < container.inventorySlots.size()) {
             TweakProvider tweakProvider = CraftingTweaks.instance.getProvider(container);
             if (tweakProvider != null) {
-                Slot slot = container.inventorySlots.get(message.slotNumber);
+                Slot slot = (Slot) container.inventorySlots.get(message.slotNumber);
                 ItemStack itemStack = slot.getStack();
                 if(itemStack != null && slot.canTakeStack(entityPlayer)) {
                     ItemStack restStack = tweakProvider.transferIntoGrid(entityPlayer, container, message.id, itemStack);
+                    if(restStack == null) {
+                        restStack = itemStack.copy();
+                        restStack.stackSize = 0;
+                    }
                     if(restStack.stackSize != itemStack.stackSize) {
                         slot.onSlotChange(restStack, itemStack);
                         ItemStack movedStack = itemStack.copy();
