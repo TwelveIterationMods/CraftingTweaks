1a83bdc55edc9c6546aa1358c74ec4defb65777a
Blay09 <blay09@hotmail.de>
2017-03-19 14:56:57 +0100

    Add JACB support to 1.10.2 #95

--- src/main/java/net/blay09/mods/craftingtweaks/addons/CraftingTweaksAddons.java
+++ src/main/java/net/blay09/mods/craftingtweaks/addons/CraftingTweaksAddons.java
@@ -12,68 +12,76 @@ import org.apache.logging.log4j.Logger;
 
 public class CraftingTweaksAddons {
 
-    public static final Logger logger = LogManager.getLogger();
+	public static final Logger logger = LogManager.getLogger();
 
-    public static void postInit(FMLPostInitializationEvent event) {
-        ezstorage();
-        thaumcraft();
-        progressiveautomation();
+	public static void postInit(FMLPostInitializationEvent event) {
+		jacb();
+		ezstorage();
+		thaumcraft();
+		progressiveautomation();
 
-        if(Loader.isModLoaded("storagesilo")) {
-            registerProvider("uk.binarycraft.storagesilo.blocks.craftingsilo.ContainerCraftingSilo", new ProviderCraftingSilo());
-        }
-    }
+		if (Loader.isModLoaded("storagesilo")) {
+			registerProvider("uk.binarycraft.storagesilo.blocks.craftingsilo.ContainerCraftingSilo", new ProviderCraftingSilo());
+		}
+	}
 
-    private static void ezstorage() {
-        SimpleTweakProvider provider = registerSimpleProvider("ezstorage", "com.zerofall.ezstorage.container.ContainerStorageCoreCrafting");
-        if(provider != null) {
-            provider.setGrid(73, 9);
-            provider.setTweakRotate(true, true, 0, 0);
-            provider.setTweakBalance(true, true, 0, 0);
-            provider.setTweakClear(true, true, 0, 0);
-            provider.setAlignToGrid(EnumFacing.WEST);
-        }
-    }
+	private static void jacb() {
+		SimpleTweakProvider provider = registerSimpleProvider("JACB", "tv.vanhal.jacb.gui.BenchContainer");
+		if (provider != null) {
+			provider.setAlignToGrid(EnumFacing.WEST);
+		}
+	}
 
-    private static void thaumcraft() {
-        SimpleTweakProvider provider = registerSimpleProvider("Thaumcraft", "thaumcraft.common.container.ContainerArcaneWorkbench");
-        if(provider != null) {
-            provider.setGrid(2, 9);
-            provider.setTweakRotate(true, true, -12, 46);
-            provider.setTweakBalance(true, true, -12, 64);
-            provider.setTweakClear(true, true, -12, 82);
-        }
-    }
+	private static void ezstorage() {
+		SimpleTweakProvider provider = registerSimpleProvider("ezstorage", "com.zerofall.ezstorage.container.ContainerStorageCoreCrafting");
+		if (provider != null) {
+			provider.setGrid(73, 9);
+			provider.setTweakRotate(true, true, 0, 0);
+			provider.setTweakBalance(true, true, 0, 0);
+			provider.setTweakClear(true, true, 0, 0);
+			provider.setAlignToGrid(EnumFacing.WEST);
+		}
+	}
 
-    private static void progressiveautomation() {
-        SimpleTweakProvider provider = registerSimpleProvider("progressiveautomation", "com.vanhal.progressiveautomation.gui.container.ContainerCrafter");
-        if(provider != null) {
-            provider.setGrid(2, 9);
-            provider.setTweakRotate(true, true, 0, 0);
-            provider.setTweakBalance(false, false, 0, 0);
-            provider.setTweakClear(true, true, 0, 0);
-            provider.setAlignToGrid(EnumFacing.WEST);
-        }
-    }
+	private static void thaumcraft() {
+		SimpleTweakProvider provider = registerSimpleProvider("Thaumcraft", "thaumcraft.common.container.ContainerArcaneWorkbench");
+		if (provider != null) {
+			provider.setGrid(2, 9);
+			provider.setTweakRotate(true, true, -12, 46);
+			provider.setTweakBalance(true, true, -12, 64);
+			provider.setTweakClear(true, true, -12, 82);
+		}
+	}
 
-    @SuppressWarnings("unchecked")
-    private static SimpleTweakProvider registerSimpleProvider(String modid, String className) {
-        try {
-            if(Loader.isModLoaded(modid)) {
-                return CraftingTweaksAPI.registerSimpleProvider(modid, (Class<? extends Container>) Class.forName(className));
-            }
-        } catch (ClassNotFoundException e) {
-            logger.error("Could not register Crafting Tweaks addon for {} - internal names have changed.", modid);
-        }
-        return null;
-    }
+	private static void progressiveautomation() {
+		SimpleTweakProvider provider = registerSimpleProvider("progressiveautomation", "com.vanhal.progressiveautomation.gui.container.ContainerCrafter");
+		if (provider != null) {
+			provider.setGrid(2, 9);
+			provider.setTweakRotate(true, true, 0, 0);
+			provider.setTweakBalance(false, false, 0, 0);
+			provider.setTweakClear(true, true, 0, 0);
+			provider.setAlignToGrid(EnumFacing.WEST);
+		}
+	}
 
-    @SuppressWarnings("unchecked")
-    private static void registerProvider(String className, TweakProvider provider) {
-        try {
-            CraftingTweaksAPI.registerProvider((Class<? extends Container>) Class.forName(className), provider);
-        } catch (ClassNotFoundException e) {
-            logger.error("Could not register Crafting Tweaks addon for {} - internal names have changed.", provider.getModId());
-        }
-    }
+	@SuppressWarnings("unchecked")
+	private static SimpleTweakProvider registerSimpleProvider(String modid, String className) {
+		try {
+			if (Loader.isModLoaded(modid)) {
+				return CraftingTweaksAPI.registerSimpleProvider(modid, (Class<? extends Container>) Class.forName(className));
+			}
+		} catch (ClassNotFoundException e) {
+			logger.error("Could not register Crafting Tweaks addon for {} - internal names have changed.", modid);
+		}
+		return null;
+	}
+
+	@SuppressWarnings("unchecked")
+	private static void registerProvider(String className, TweakProvider provider) {
+		try {
+			CraftingTweaksAPI.registerProvider((Class<? extends Container>) Class.forName(className), provider);
+		} catch (ClassNotFoundException e) {
+			logger.error("Could not register Crafting Tweaks addon for {} - internal names have changed.", provider.getModId());
+		}
+	}
 }
