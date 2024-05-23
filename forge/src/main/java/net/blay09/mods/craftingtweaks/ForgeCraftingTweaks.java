9535e7c7dd6a491550e06c60c55f2626631633b5
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2024-05-23 15:52:43 +0200

    fix(1.20.6): Fix Forge builds

--- forge/src/main/java/net/blay09/mods/craftingtweaks/ForgeCraftingTweaks.java
+++ forge/src/main/java/net/blay09/mods/craftingtweaks/ForgeCraftingTweaks.java
@@ -1,6 +1,7 @@
 package net.blay09.mods.craftingtweaks;
 
 import net.blay09.mods.balm.api.Balm;
+import net.blay09.mods.balm.api.EmptyLoadContext;
 import net.blay09.mods.balm.api.client.BalmClient;
 import net.blay09.mods.craftingtweaks.client.CraftingTweaksClient;
 import net.minecraftforge.api.distmarker.Dist;
@@ -13,8 +14,8 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 @Mod(CraftingTweaks.MOD_ID)
 public class ForgeCraftingTweaks {
     public ForgeCraftingTweaks() {
-        Balm.initialize(CraftingTweaks.MOD_ID, CraftingTweaks::initialize);
-        DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> BalmClient.initialize(CraftingTweaks.MOD_ID, CraftingTweaksClient::initialize));
+        Balm.initialize(CraftingTweaks.MOD_ID, EmptyLoadContext.INSTANCE, CraftingTweaks::initialize);
+        DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> BalmClient.initialize(CraftingTweaks.MOD_ID, EmptyLoadContext.INSTANCE, CraftingTweaksClient::initialize));
 
         FMLJavaModLoadingContext.get().getModEventBus().addListener(IMCHandler::processInterMod);
         ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> IExtensionPoint.DisplayTest.IGNORESERVERONLY, (a, b) -> true));
