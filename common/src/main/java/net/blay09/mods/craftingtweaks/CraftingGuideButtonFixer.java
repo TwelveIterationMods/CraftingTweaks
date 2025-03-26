f72388eac1d9b325ad2e83135c04c4a55081bd2a
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-03-26 09:29:02 +0100

    feat: Add GridGuiHandler.hideRecipeBookButton, GridGuiHandler.repositionRecipeBookButton, GridGuiHandler.findRecipeBookButton

--- common/src/main/java/net/blay09/mods/craftingtweaks/CraftingGuideButtonFixer.java
+++ common/src/main/java/net/blay09/mods/craftingtweaks/CraftingGuideButtonFixer.java
@@ -1,41 +1,22 @@
 package net.blay09.mods.craftingtweaks;
 
+import net.blay09.mods.craftingtweaks.api.GridGuiHandler;
 import net.blay09.mods.craftingtweaks.config.CraftingTweaksConfig;
-import net.blay09.mods.craftingtweaks.config.CraftingTweaksConfigData;
-import net.blay09.mods.balm.mixin.AbstractContainerScreenAccessor;
-import net.blay09.mods.balm.mixin.ImageButtonAccessor;
-import net.blay09.mods.balm.mixin.ScreenAccessor;
-import net.minecraft.client.gui.components.Button;
-import net.minecraft.client.gui.components.events.GuiEventListener;
+import net.minecraft.client.gui.components.AbstractWidget;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
-import net.minecraft.client.gui.screens.inventory.CraftingScreen;
-import org.jetbrains.annotations.Nullable;
-
-import java.util.List;
 
 public class CraftingGuideButtonFixer {
 
-    public static Button fixMistakes(AbstractContainerScreen<?> screen) {
-        Button button = findCraftButton(((ScreenAccessor) screen).balm_getChildren());
-        if (button != null) {
-            CraftingTweaksConfigData config = CraftingTweaksConfig.getActive();
-            if (config.client.hideVanillaCraftingGuide) {
-                button.visible = false;
-            } else if (screen instanceof CraftingScreen) {
-                AbstractContainerScreenAccessor accessor = (AbstractContainerScreenAccessor) screen;
-                button.setX(accessor.getLeftPos() + accessor.getImageWidth() - 25);
-                button.setY(accessor.getTopPos() + 5);
+    public static AbstractWidget fixMistakes(AbstractContainerScreen<?> screen, GridGuiHandler guiHandler) {
+        final var opt = guiHandler.findRecipeBookButton(screen);
+        opt.ifPresent(button -> {
+            if (CraftingTweaksConfig.getActive().client.hideVanillaCraftingGuide) {
+                guiHandler.hideRecipeBookButton(screen, button);
+            } else {
+                guiHandler.repositionRecipeBookButton(screen, button);
             }
-        }
-        return button;
-    }
-
-    @Nullable
-    private static Button findCraftButton(List<? extends GuiEventListener> buttonList) {
-        return (Button) buttonList
-                .stream()
-                .filter(p -> p instanceof ImageButtonAccessor imageButton && imageButton.getSprites().get(false, false).getPath().equals("recipe_book/button"))
-                .findFirst().orElse(null);
+        });
+        return opt.orElse(null);
     }
 
 }
