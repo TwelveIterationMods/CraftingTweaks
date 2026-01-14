ca93fb0fa9377ad99672386757f788fd723ea02a
Ryvu1 <247581399+Ryvu1@users.noreply.github.com>
2026-01-14 16:59:54 +0100

    fix: Fix slot.setChanged not being called when changing Slot content #257 (#258)

--- common/src/main/java/net/blay09/mods/craftingtweaks/network/CompressMessage.java
+++ common/src/main/java/net/blay09/mods/craftingtweaks/network/CompressMessage.java
@@ -146,6 +146,7 @@ public class CompressMessage implements CustomPacketPayload {
             int itemsToRemove = craftsPossible * recipeSize;
             giveLeftoverItems(player, mouseStack, itemsToRemove);
             mouseStack.shrink(itemsToRemove);
+            mouseSlot.setChanged();
 
             // Add crafted items to the inventory
             addCraftedItemsToInventory(player, recipe.result(), craftsPossible);
